b5082e8805d60dc3873c1b091b3b885c821126a8
Daniel <dkbaffour777@gmail.com>
2021-04-09 15:34:44 -0700

    add current day, add event blocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,3 +15,31 @@ var day = date.format("DD")
 var currentDay = dayName + ", " + month + " " + day
 
 $("#currentDay").text(currentDay)
+
+var eventContEl = $("#event-container")
+
+var eventTemplate = ``
+var businessHours = [9, 10, 11, 12, 1, 2, 3, 4, 5]
+
+for (let index = 0; index < 9; index++) {
+    var meridiem =  (businessHours[index] >= 9 && businessHours[index] <= 12) ? "AM" : "PM"
+    eventTemplate += `
+        <div id="event-cont" class="row d-flex align-items-stretch" data-event-id="` + index + `">
+            <div class="col-1 d-flex align-items-center justify-content-end">
+                <p id="time">` 
+                    + businessHours[index] + meridiem + 
+                `</p>
+            </div>
+            <div class="col-10 px-0 d-flex align-items-center justify-content-center">
+                <textarea id="event-title" class="border-0 w-100 h-100 p-4 present">Change Event</textarea>
+            </div>
+            <div class="col-1 d-flex align-items-center justify-content-center saveBtn">
+                <i id="saveBtn" class="fas fa-save"></i>
+            </div>
+        </div>
+    `    
+}
+
+eventContEl.html(eventTemplate)
+
+
